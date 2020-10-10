1d2b3828d06af7cc895794b64196e53262d5e916
ct4433 <332386415@qq.com>
2020-10-11 00:38:37 +0800

    vue3-toolbar

---
+++ src/components/tabbar/MainTabBar.vue
@@ -0,0 +1,81 @@
+<template>
+  <TabBar>
+    <TabBarItem path="/home" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="" />
+      </template>
+      <template v-slot:item-icon-active>
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/home_active.png"
+          alt=""
+        />
+      </template>
+      <template v-slot:item-text>
+        <div slot="item-text">首页</div>
+      </template>
+    </TabBarItem>
+
+    <TabBarItem path="/category" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img slot="item-icon" src="~assets/img/tabbar/categories.png" alt="" />
+      </template>
+      <template v-slot:item-icon-active>
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/categories_active.png"
+          alt=""
+        />
+      </template>
+      <template v-slot:item-text>
+        <div slot="item-text">分类</div>
+      </template>
+    </TabBarItem>
+
+    <TabBarItem path="/cart" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.png" alt="" />
+      </template>
+      <template v-slot:item-icon-active>
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/shopcart_active.png"
+          alt=""
+        />
+      </template>
+      <template v-slot:item-text>
+        <div slot="item-text">购物车</div>
+      </template>
+    </TabBarItem>
+
+    <TabBarItem path="/profile" activeColor="deepPink">
+      <template v-slot:item-icon>
+        <img slot="item-icon" src="~assets/img/tabbar/profile.png" alt="" />
+      </template>
+      <template v-slot:item-icon-active>
+        <img
+          slot="item-icon-active"
+          src="~assets/img/tabbar/profile_active.png"
+          alt=""
+        />
+      </template>
+      <template v-slot:item-text>
+        <div slot="item-text">我的</div>
+      </template>
+    </TabBarItem>
+  </TabBar>
+</template>
+
+<script>
+import TabBar from "components/tabbar/TabBar";
+import TabBarItem from "components/tabbar/TabBarItem";
+export default {
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="tab-bar">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "../../assets/css/tabbar/tabbar.css";
+</style>
